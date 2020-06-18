27b3223c9d38833622ab834070abaa229c930611
Yashvardhan Mulki <yash.mulki@hotmail.com>
2020-06-18 17:28:47 -0400

    Fixed gitignore configuration

---
+++ cmake-build-debug/CMakeFiles/Hadamard.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hadamard.dir/main.cpp.o"
+  "Hadamard.pdb"
+  "Hadamard"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Hadamard.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
